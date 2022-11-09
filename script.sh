9f92300e04c3ebebe027b4c83d0591d14125db97
Your Name <you@example.com>
2022-11-09 20:28:45 +0000

    Menu is finished

--- script.sh
+++ script.sh
@@ -1,7 +1,20 @@
 #TODO: Menú del script
 clear
+while :
+do
+echo ""
+echo ""
 echo "MENU SCRIPT v.0.1"
 echo "================="
 echo "1. Mostrar contenido directorio"
 echo "2. Salir"
-
+echo -n "Escoger opcion: "
+read opcion
+case $opcion in
+	1) echo "Mostrar contenido directorio"
+	ls;;
+	2) exit 0;;
+	*) echo "Opcion invalida"
+	sleep 1;;
+esac
+done
